11b92b8641e58e217cc902c68db736708623b64c
Goran Lovincic <66091550+Goran1310@users.noreply.github.com>
2023-01-11 09:41:15 +0000

    Pending changes exported from your codespace

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <header>
-    <div class="logos">
-      <img
-        v-if="isDesktop"
-        alt="NW.js logo"
-        src="./assets/nw.png"
-      />
-      <img
-        alt="Vue logo"
-        src="./assets/logo.svg"
-      />
-    </div>
 
     <div class="wrapper">
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
       <HelloWorld :message="message" />
       <hr />
       <div class="flex">
@@ -62,6 +75,7 @@ header {
   display: block;
   text-align: center;
 }
+
 .logos img {
   max-width: 125px;
   max-height: 125px;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import applyPrototypes from '@/helpers/applyPrototypes.js';
 
 import App from '@/App.vue';
-
 import '@/assets/main.css';
+
+
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 const app = createApp(App);
 applyPrototypes(app.config.globalProperties);
